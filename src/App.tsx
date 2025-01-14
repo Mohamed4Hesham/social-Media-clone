06b56340bfb00c4df3642682ed38f3a9d6746113
Memy11th <abdelrahmanabdue11@gmail.com>
2025-01-15 00:05:06 +0300

    Browser Router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
+import Layout from './pages/Layout/Layout'
+import Register from './pages/Register/Register'
+
 
 function App() {
 
+  const Routes = createBrowserRouter([
+    {path:'', element:<Layout/>,children:[
+      {path:'/register', element:<Register/>},
+    ]}
+  ])
   return (
     <>
+      <RouterProvider router={Routes}>
+
+      </RouterProvider>
       
     </>
   )
