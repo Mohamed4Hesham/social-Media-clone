75fa52738c84c2e5b02e87712124a5412bf20b5e
Mohamed4Hesham <mohamedhesham4423@gmail.com>
2025-01-18 23:51:42 +0200

    Navbar phase 1

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,10 +1,11 @@
 import { Outlet } from 'react-router-dom'
+import Navbar from './navBar/navbar'
 const Layout = () => {
     return <>
     {/* Navbar*/ }
+    <Navbar/>
     <div className='min-h-screen container mx-auto  p-2'>
     <Outlet>
-
     </Outlet>
     </div>
 
