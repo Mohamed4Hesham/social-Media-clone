33a6c28276e0f851fe68f222da9a5127ada1490b
Memy11th <abdelrahmanabdue11@gmail.com>
2025-01-18 12:51:02 +0300

    Metadata added to Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Helmet } from 'react-helmet-async'
 
 const Home = () => {
 
   return <>
+    <Helmet>
+      <title>Home</title>
+      <meta name="description" content="News feed to your account on our social media platform , have fun mi amigo" />
+    </Helmet>
       ze2b 
     </>
 }
