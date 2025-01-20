eb9fcbba8f7190c0e9e00d0150ebb6e374c73eee
Mohamed4Hesham <mohamedhesham4423@gmail.com>
2025-01-21 00:34:35 +0200

    resetPassworx setup

--- src/pages/Settings/ChangePassword/ChangePassword.tsx
+++ src/pages/Settings/ChangePassword/ChangePassword.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const ChangePassword = () => {
     return (
-        <div>
-        jaja
-        </div>
+    <div className="w-20 m-auto mt-40 bg-slate-700 text-white text-center">
+        <form>
+            
+        </form>
+    </div>
+
     )
 }
 
