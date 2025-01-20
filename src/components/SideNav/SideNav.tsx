bc67fb06eae86106ad0f4774f576cc40e38cf161
Memy11th <abdelrahmanabdue11@gmail.com>
2025-01-20 21:54:11 +0300

    Done

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -5,14 +5,13 @@ const SideNav = () => {
         <div className=' flex flex-col justify-center col-span-3 shadow-xl rounded-2xl min-h-screen bg-slate-100'>
             <h2 className='text-2xl font-bold text-center mb-4 p-2 text-slate-700'>Settings</h2>
             <ul>
-                <li className='text-slate-700 text-base underline'>
+                <li className='text-slate-700 text-base '>
                     <NavLink to="">Reset password</NavLink>
                 </li>
-                <li className='text-slate-700 text-base underline'>
+                <li className='text-slate-700 text-base '>
                     <NavLink to="ChangeProfilePic">Reset password</NavLink>
                 </li>
             </ul>
-            <p className='text-slate-700 text-base underline'> Reset password </p>
 
         </div>
     </>
