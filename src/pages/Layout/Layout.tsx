7553b5fbb367c1417c17876cf39688aa92d2dc76
Memy11th <abdelrahmanabdue11@gmail.com>
2025-01-20 23:50:15 +0300

    Hola mi amigo

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,14 +1,16 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Navbar from './Navbar/Navbar'
 import React from 'react';
 const Layout = () => {
+    const location = useLocation();
+    const pathname = location.pathname;
     React.useEffect(() => {
         window.scrollTo(0, 0); // Scroll to the top on page load
     }, []);
     return <>
     {/* Navbar*/ }
     <Navbar/>
-    <div className='min-h-screen container mx-auto  p-2'>
+    <div className={`min-h-screen ${pathname === '/settings' || pathname === '/settings/ChangeProfilePic' || pathname === '/settings/ChangePassword'  ? '' : 'container' } mx-auto  p-2`}>
     <Outlet>
     </Outlet>
     </div>
