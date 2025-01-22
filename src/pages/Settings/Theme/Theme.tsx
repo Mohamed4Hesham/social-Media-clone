98d0f372696be05dceafbd2986aebdad9059a8ef
Memy11th <abdelrahmanabdue11@gmail.com>
2025-01-23 00:52:06 +0300

    colors for darkmode

--- src/pages/Settings/Theme/Theme.tsx
+++ src/pages/Settings/Theme/Theme.tsx
@@ -4,8 +4,8 @@ const Theme = () => {
     return <>
     <p className=' text-center mb-8 font-bold text-gray-800 text-xl '>Theme toggle</p>
     <div className='flex flex-col mx-8 min-h-screen justify-start '> 
-        <div className='flex items-center justify-between '>
-        <p className='text-center font-bold text-yellow-400 dark:text-gray-700  '>Theme mode</p>
+        <div className='flex items-center justify-between bg-slate-200/60 dark:bg-slate-300/50 p-4  rounded-2xl '>
+        <p className='text-center font-bold  dark:text-gray-700  '>Theme mode</p>
         <ThemeToggle />
         </div>
         
