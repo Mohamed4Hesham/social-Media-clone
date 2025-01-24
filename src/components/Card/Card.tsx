453bc9229167708d1989b90f4445e8b462d3eb50
Memy11th <abdelrahmanabdue11@gmail.com>
2025-01-24 22:16:22 +0300

    Profile layout added + Profile avatar + username and email with skeleton effect for loading added

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,7 +1,9 @@
 
 const Card = () => {
     return <>
-    
+        <div className="p-2 bg-red-800  col-span-10 col-start-2  ">
+            Hola
+        </div>
     </>
 }
 
--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,3 +1,5 @@
+import Card from '@/components/Card/Card';
+import GridDynamicContainer from '@/components/Grid container/GridDynamicContainer';
 import { LoggedUserData } from '@/interfaces/getLoggedUserData';
 import { getLoggedUserData } from '@/redux/slices/UserSlice';
 import { AppDispatch } from '@/redux/Store';
@@ -33,12 +35,44 @@ const Profile = () => {
         };
         getData();
     },[dispatch]);
+    console.log(userData);
     return <>
     <Helmet>
         <title>{userData.user.name.length > 2 ? `${userData.user.name}'s Profile` : "Profile"}</title>
         <meta name="description" content={`Profile to our lovely user ${userData.user.name}`} />
     </Helmet>
-        <h1>{userData.user.name}</h1>
+        <GridDynamicContainer cols={12} className=' ' >
+                <div className="p-2 col-span-10 col-start-2">
+        {userData.user.name.length <= 2 ? (
+            <div className="animate-pulse">
+            <div className="flex items-center mb-4">
+                <div className="w-12 h-12 bg-gray-300 rounded-full mr-4"></div>
+                <div>
+                <div className="h-4 bg-gray-300 rounded w-32 mb-2"></div>
+                <div className="h-4 bg-gray-300 rounded w-48"></div>
+                </div>
+            </div>
+            </div>
+        ) : (
+            <>
+            
+            <div className="flex items-center mb-4">
+                <img
+                className="w-12 h-12 rounded-full mr-4"
+                src={userData.user.photo}
+                alt={userData.user.name}
+                />
+                <div>
+                <h3 className="text-lg font-semibold">{userData.user.name}</h3>
+                <p className="text-gray-600">{userData.user.email}</p>
+                </div>
+            </div>
+            </>
+        )}
+        </div>
+
+                        <Card />
+        </GridDynamicContainer>
     </>
 }
 
